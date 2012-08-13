9ce78997f5a81ba491226c4539d2ee1fbceb6798
John Ericson <Ericson2314@Yahoo.com>
2012-08-13 14:08:01 -0700

    First C alternative for 'updatereflects' now in use

--- source/voxlap5.cpp
+++ source/voxlap5.cpp
@@ -10070,33 +10070,10 @@ static void updatereflects (vx5sprite *spr)
 		if (i > 2047) i = 2047;
 		fogmul = foglut[i];
 
-	#ifdef NOASM
 		i = (long)(*(short *)&fogmul);
 		((short *)kv6coladd)[0] = (short)((((long)(((short *)&fogcol)[0]))*i)>>1);
 		((short *)kv6coladd)[1] = (short)((((long)(((short *)&fogcol)[1]))*i)>>1);
 		((short *)kv6coladd)[2] = (short)((((long)(((short *)&fogcol)[2]))*i)>>1);
-	#else
-		#ifdef __GNUC__ //gcc inline asm
-		__asm__ __volatile__
-		(
-			"paddd	%[m64], %[m64]\n"
-			"pmulhuw	%[mul], %[m64]\n"
-			: [m64] "=y" (*(lpoint2d *)kv6coladd)
-			: "0" (fogcol), [mul] "m" (fogmul)
-			:
-		);
-		#endif
-		#ifdef _MSC_VER //msvc inline asm
-		_asm
-		{
-			movq	mm0, fogcol
-			paddd	mm0, mm0
-			pmulhuw	mm0, fogmul
-			movq	kv6coladd[0], mm0
-			emms
-		}
-		#endif
-	#endif
 	}
 	else
 	{
