ea7c063a9874726117a67ac52a4552986ea88c2d
John Ericson <Ericson2314@Yahoo.com>
2013-01-03 19:23:01 -0500

    mmxcolorsub & mmxcoloradd now C in voxflash

--- include/voxflash.h
+++ include/voxflash.h
@@ -30,58 +30,18 @@ void mmxcolorsub (long *);
 
 static inline void mmxcoloradd (long *a)
 {
-	#ifdef NOASM
 	((uint8_t *)a)[0] += ((uint8_t *)flashbrival)[0];
 	((uint8_t *)a)[1] += ((uint8_t *)flashbrival)[1];
 	((uint8_t *)a)[2] += ((uint8_t *)flashbrival)[2];
 	((uint8_t *)a)[3] += ((uint8_t *)flashbrival)[3];
-	#endif
-	#ifdef __GNUC__ //gcc inline asm
-	__asm__ __volatile__
-	(
-		"paddusb	%c[fb], %[a]\n"
-		: [a] "+y" (*((uint32_t*)a))
-		: [fb] "p" (&flashbrival)
-		:
-	);
-	#endif
-	#ifdef _MSC_VER //msvc inline asm
-	_asm
-	{
-		mov	eax, a
-		movd	mm0, [eax]
-		paddusb	mm0, flashbrival
-		movd	[eax], mm0
-	}
-	#endif
 }
 
 static inline void mmxcolorsub (long *a)
 {
-	#ifdef NOASM
 	((uint8_t *)a)[0] -= ((uint8_t *)flashbrival)[0];
 	((uint8_t *)a)[1] -= ((uint8_t *)flashbrival)[1];
 	((uint8_t *)a)[2] -= ((uint8_t *)flashbrival)[2];
 	((uint8_t *)a)[3] -= ((uint8_t *)flashbrival)[3];
-	#endif
-	#ifdef __GNUC__ //gcc inline asm
-	__asm__ __volatile__
-	(
-		"psubusb	%c[fb], %[a]\n"
-		: [a] "+y" (*((uint32_t*)a))
-		: [fb] "p" (&flashbrival)
-		:
-	);
-	#endif
-	#ifdef _MSC_VER //msvc inline asm
-	_asm
-	{
-		mov	eax, a
-		movd	mm0, [eax]
-		psubusb	mm0, flashbrival
-		movd	[eax], mm0
-	}
-	#endif
 }
 
 #endif
