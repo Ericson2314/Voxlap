d80e4964e220525dea11694a527dc7c14f749abf
Lensman <plasmic.origin@gmail.com>
2013-04-04 22:54:21 +0100

    Spiltting Voxlap5.cpp into smaller, related components

--- source/voxlap5.cpp
+++ source/voxlap5.cpp
@@ -41,10 +41,12 @@
 
 	//mmxcolor* now defined here
 #include "voxflash.h"
-#include "kfonts.h"
+
 	//KPlib Preprocessor stuff
 //#define KPLIB_C  //if kplib is compiled as C
 #include "kplib.h"
+#include "kfonts.h"
+#include "kcolors.h"
 
 #define USEZBUFFER 1                 //Should a Z-Buffer be Used?
 //#define NOASM                  //Instructs compiler to use C(++) alternatives
@@ -60,6 +62,7 @@ EXTERN_SYSMAIN void breath ();
 EXTERN_SYSMAIN void evilquit (const char *);
 
 #define VOXSIZ VSID*VSID*128
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -376,62 +379,7 @@ void print6x8 (long x, long y, long fcol, long bcol, const char *fmt, ...)
 			if ((*c) == 9) { for(i=24;i<72;i+=4) *(long *)(x+i) = bcol; x += ((2*6)<<2); }
 		}
 }
-
-static long gkrand = 0;
-/** Jitter the colour in i by random jitamount
- *  @param i Acolour
- *  @param jitamount Amount of jitterage
- *  @return colorjit Jittered colour
-*/
-long colorjit (long i, long jitamount)
-{
-	gkrand = (gkrand*27584621)+1;
-	return((gkrand&jitamount)^i);
-}
-
-/** Brighten a given colour */
-long lightvox (long i)
-{
-	long r, g, b;
-
-	b = ((unsigned long)i>>24);
-	r = MIN((((i>>16)&255)*b)>>7,255);
-	g = MIN((((i>>8 )&255)*b)>>7,255);
-	b = MIN((((i    )&255)*b)>>7,255);
-	return((r<<16)+(g<<8)+b);
-}
-
-	//Note: ebx = 512 is no change
-	//If PENTIUM III:1.Replace punpcklwd&punpckldq with: pshufw mm1, mm1, 0
-	//               2.Use pmulhuw, shift by 8 & mul by 256
-	//  :(  Can't mix with floating point
-//#pragma aux colormul =
-//   "movd mm0, eax"
-//   "pxor mm1, mm1"
-//   "punpcklbw mm0, mm1"
-//   "psllw mm0, 7"
-//   "movd mm1, ebx"
-//   "punpcklwd mm1, mm1"
-//   "punpckldq mm1, mm1"
-//   "pmulhw mm0, mm1"
-//   "packsswb mm0, mm0"
-//   "movd eax, mm0"
-//   parm [eax][ebx]
-//   modify exact [eax]
-//   value [eax]
-
-long colormul (long i, long mulup8)
-{
-	long r, g, b;
-
-	r = ((((i>>16)&255)*mulup8)>>8); if (r > 255) r = 255;
-	g = ((((i>>8 )&255)*mulup8)>>8); if (g > 255) g = 255;
-	b = ((((i    )&255)*mulup8)>>8); if (b > 255) b = 255;
-	return((i&0xff000000)+(r<<16)+(g<<8)+b);
-}
-
-long curcolfunc (lpoint3d *p) { return(vx5.curcol); }
-
+/** This should move to kcolors.h , but need to deal with global sptr first */
 long floorcolfunc (lpoint3d *p)
 {
 	char *v;
@@ -439,160 +387,6 @@ long floorcolfunc (lpoint3d *p)
 	return(*(long *)&v[4]);
 }
 
-long jitcolfunc (lpoint3d *p) { return(colorjit(vx5.curcol,vx5.amount)); }
-
-static long manycolukup[64] =
-{
-	  0,  1,  2,  5, 10, 15, 21, 29, 37, 47, 57, 67, 79, 90,103,115,
-	127,140,152,165,176,188,198,208,218,226,234,240,245,250,253,254,
-	255,254,253,250,245,240,234,226,218,208,198,188,176,165,152,140,
-	128,115,103, 90, 79, 67, 57, 47, 37, 29, 21, 15, 10,  5,  2,  1
-};
-long manycolfunc (lpoint3d *p)
-{
-	return((manycolukup[p->x&63]<<16)+(manycolukup[p->y&63]<<8)+manycolukup[p->z&63]+0x80000000);
-}
-
-long sphcolfunc (lpoint3d *p)
-{
-	long i;
-	ftol(sin((p->x+p->y+p->z-vx5.cen)*vx5.daf)*-96,&i);
-	return(((i+128)<<24)|(vx5.curcol&0xffffff));
-}
-
-#define WOODXSIZ 46
-#define WOODYSIZ 24
-#define WOODZSIZ 24
-static float wx[256], wy[256], wz[256], vx[256], vy[256], vz[256];
-long woodcolfunc (lpoint3d *p)
-{
-	float col, u, a, f, dx, dy, dz;
-	long i, c, xof, yof, tx, ty, xoff;
-
-	if (*(long *)&wx[0] == 0)
-	{
-		for(i=0;i<256;i++)
-		{
-			wx[i] = WOODXSIZ * ((float)rand()/32768.0f-.5f) * .5f;
-			wy[i] = WOODXSIZ * ((float)rand()/32768.0f-.5f) * .5f;
-			wz[i] = WOODXSIZ * ((float)rand()/32768.0f-.5f) * .5f;
-
-				//UNIFORM spherical randomization (see spherand.c)
-			dz = 1.0f-(float)rand()/32768.0f*.04f;
-			a = (float)rand()/32768.0f*PI*2.0f; fcossin(a,&dx,&dy);
-			f = sqrt(1.0f-dz*dz); dx *= f; dy *= f;
-				//??z: rings,  ?z?: vertical,  z??: horizontal (nice)
-			vx[i] = dz; vy[i] = fabs(dy); vz[i] = dx;
-		}
-	}
-
-		//(tx&,ty&) = top-left corner of current panel
-	ty = p->y - (p->y%WOODYSIZ);
-	xoff = ((ty/WOODYSIZ)*(ty/WOODYSIZ)*51721 + (p->z/WOODZSIZ)*357) % WOODXSIZ;
-	tx = ((p->x+xoff) - (p->x+xoff)%WOODXSIZ) - xoff;
-
-	xof = p->x - (tx + (WOODXSIZ>>1));
-	yof = p->y - (ty + (WOODYSIZ>>1));
-
-	c = ((((tx*429 + 4695) ^ (ty*341 + 4355) ^ 13643) * 2797) & 255);
-	dx = xof - wx[c];
-	dy = yof - wy[c];
-	dz = (p->z%WOODZSIZ) - wz[c];
-
-		//u = distance to center of randomly oriented cylinder
-	u = vx[c]*dx + vy[c]*dy + vz[c]*dz;
-	u = sqrt(dx*dx + dy*dy + dz*dz - u*u);
-
-		//ring randomness
-	u += sin((float)xof*.12 + (float)yof*.15) * .5;
-	u *= (sin(u)*.05 + 1);
-
-		//Ring function: smooth saw-tooth wave
-	col = sin(u*2)*24;
-	col *= pow(1.f-vx[c],.3f);
-
-		//Thin shaded borders
-	if ((p->x-tx == 0) || (p->y-ty == 0)) col -= 5;
-	if ((p->x-tx == WOODXSIZ-1) || (p->y-ty == WOODYSIZ-1)) col -= 3;
-
-	//f = col+c*.12+72; i = ftolp3(&f);
-	  ftol(col+c*.12f+72.0f,&i);
-
-	return(colormul(vx5.curcol,i<<1));
-}
-
-long gxsizcache = 0, gysizcache = 0;
-long pngcolfunc (lpoint3d *p)
-{
-	long x, y, z, u, v;
-	float fx, fy, fz, rx, ry, rz;
-
-	if (!vx5.pic) return(vx5.curcol);
-	switch(vx5.picmode)
-	{
-		case 0:
-			x = p->x-vx5.pico.x; y = p->y-vx5.pico.y; z = p->z-vx5.pico.z;
-			u = (((x&vx5.picu.x) + (y&vx5.picu.y) + (z&vx5.picu.z))^vx5.xoru);
-			v = (((x&vx5.picv.x) + (y&vx5.picv.y) + (z&vx5.picv.z))^vx5.xorv);
-			break;
-		case 1: case 2:
-			fx = (float)p->x-vx5.fpico.x;
-			fy = (float)p->y-vx5.fpico.y;
-			fz = (float)p->z-vx5.fpico.z;
-			rx = vx5.fpicu.x*fx + vx5.fpicu.y*fy + vx5.fpicu.z*fz;
-			ry = vx5.fpicv.x*fx + vx5.fpicv.y*fy + vx5.fpicv.z*fz;
-			rz = vx5.fpicw.x*fx + vx5.fpicw.y*fy + vx5.fpicw.z*fz;
-			ftol(atan2(ry,rx)*vx5.xoru/(PI*2),&u);
-			if (vx5.picmode == 1) ftol(rz,&v);
-			else ftol((atan2(rz,sqrt(rx*rx+ry*ry))/PI+.5)*vx5.ysiz,&v);
-			break;
-		default: //case 3:
-			fx = (float)p->x-vx5.fpico.x;
-			fy = (float)p->y-vx5.fpico.y;
-			fz = (float)p->z-vx5.fpico.z;
-			ftol(vx5.fpicu.x*fx + vx5.fpicu.y*fy + vx5.fpicu.z*fz,&u);
-			ftol(vx5.fpicv.x*fx + vx5.fpicv.y*fy + vx5.fpicv.z*fz,&v);
-			break;
-	}
-	if (((unsigned long)u-gxsizcache) >= ((unsigned long)vx5.xsiz))
-	{
-		if (u < 0) gxsizcache = u-(u+1)%vx5.xsiz-vx5.xsiz+1;
-		else gxsizcache = u-(u%vx5.xsiz);
-	}
-	if (((unsigned long)v-gysizcache) >= ((unsigned long)vx5.ysiz))
-	{
-		if (v < 0) gysizcache = v-(v+1)%vx5.ysiz-vx5.ysiz+1;
-		else gysizcache = v-(v%vx5.ysiz);
-	}
-	return((vx5.pic[(v-gysizcache)*(vx5.bpl>>2)+(u-gxsizcache)]&0xffffff)|0x80000000);
-}
-
-	//Special case for SETSEC & SETCEI bumpmapping (vx5.picmode == 3)
-	//no safety checks, returns alpha as signed char in range: (-128 to 127)
-long hpngcolfunc (point3d *p)
-{
-	long u, v;
-	float fx, fy, fz;
-
-	fx = p->x-vx5.fpico.x;
-	fy = p->y-vx5.fpico.y;
-	fz = p->z-vx5.fpico.z;
-	ftol(vx5.fpicu.x*fx + vx5.fpicu.y*fy + vx5.fpicu.z*fz,&u);
-	ftol(vx5.fpicv.x*fx + vx5.fpicv.y*fy + vx5.fpicv.z*fz,&v);
-
-	if (((unsigned long)u-gxsizcache) >= ((unsigned long)vx5.xsiz))
-	{
-		if (u < 0) gxsizcache = u-(u+1)%vx5.xsiz-vx5.xsiz+1;
-		else gxsizcache = u-(u%vx5.xsiz);
-	}
-	if (((unsigned long)v-gysizcache) >= ((unsigned long)vx5.ysiz))
-	{
-		if (v < 0) gysizcache = v-(v+1)%vx5.ysiz-vx5.ysiz+1;
-		else gysizcache = v-(v%vx5.ysiz);
-	}
-	return(vx5.pic[(v-gysizcache)*(vx5.bpl>>2)+(u-gxsizcache)]>>24);
-}
-// --------------------------------------- END COLOR
 /**
 *   QUESS: Slab length
 */
@@ -1957,7 +1751,7 @@ void (*vrend)(long,long,long,long,long);
 
 #if (USEZBUFFER != 1) //functions without Z Buffer
 
-	// Portable C/C++
+/** Horizontal render with no zbuffer */
 void hrendnoz (long sx, long sy, long p1, long plc, long incr, long j)
 {
 	sy = ylookup[sy]+frameplace; p1 = sy+(p1<<2); sy += (sx<<2);
@@ -1967,7 +1761,7 @@ void hrendnoz (long sx, long sy, long p1, long plc, long incr, long j)
 		plc += incr; sy += 4;
 	} while (sy != p1);
 }
-
+/** Vertical render with no zbuffer */
 void vrendnoz (long sx, long sy, long p1, long iplc, long iinc)
 {
 	sy = ylookup[sy]+(sx<<2)+frameplace;
@@ -1979,7 +1773,7 @@ void vrendnoz (long sx, long sy, long p1, long iplc, long iinc)
 }
 
 #else //functions with Z Buffer
-
+/** Horizontal render with zbuffer */
 void hrendz (long sx, long sy, long p1, long plc, long incr, long j)
 {
 	long p0, i; float dirx, diry;
@@ -1995,7 +1789,7 @@ void hrendz (long sx, long sy, long p1, long plc, long incr, long j)
 		dirx += optistrx; diry += optistry; plc += incr; p0 += 4;
 	} while (p0 != p1);
 }
-
+/** Vertical render with zbuffer */
 void vrendz (long sx, long sy, long p1, long iplc, long iinc)
 {
 	float dirx, diry; long i, p0;
@@ -2011,7 +1805,7 @@ void vrendz (long sx, long sy, long p1, long iplc, long iinc)
 		dirx += optistrx; diry += optistry; uurend[sx] += uurend[sx+MAXXDIM]; p0 += 4; iplc += iinc; sx++;
 	}
 }
-
+/** Horizontal render with zbuffer + fog */
 void hrendzfog (long sx, long sy, long p1, long plc, long incr, long j)
 {
 	long p0, i, k, l; float dirx, diry;
@@ -2032,7 +1826,7 @@ void hrendzfog (long sx, long sy, long p1, long plc, long incr, long j)
 		dirx += optistrx; diry += optistry; plc += incr; p0 += 4;
 	} while (p0 != p1);
 }
-
+/** Vertical render with zbuffer + fog */
 void vrendzfog (long sx, long sy, long p1, long iplc, long iinc)
 {
 	float dirx, diry; long i, k, l, p0;
@@ -2056,24 +1850,23 @@ void vrendzfog (long sx, long sy, long p1, long iplc, long iinc)
 
 #endif
 
-/**
- * Set global camera position for future voxlap5 engine calls. Functions
- * that depend on this include: opticast, drawsprite, spherefill, etc...
+/** Set global camera position for future voxlap5 engine calls.
+ *  Functions that depend on this include: opticast, drawsprite, spherefill, etc...
  *
- * The 5th & 6th parameters define the center of the screen projection. This
- * is the point on the screen that intersects the <ipos + ifor*t> vector.
+ *  The 5th & 6th parameters define the center of the screen projection. This
+ *  is the point on the screen that intersects the <ipos + ifor*t> vector.
  *
- * The last parameter is the focal length - use it to control zoom. If you
- * want a 90 degree field of view (left to right side of screen), then
- * set it to half of the screen's width: (xdim*.5).
+ *  The last parameter is the focal length - use it to control zoom. If you
+ *  want a 90 degree field of view (left to right side of screen), then
+ *  set it to half of the screen's width: (xdim*.5).
  *
- * @param ipo camera position
- * @param ist camera's unit RIGHT vector
- * @param ihe camera's unit DOWN vector
- * @param ifo camera's unit FORWARD vector
- * @param dahx x-dimension of viewing window
- * @param dahy y-dimension of viewing window
- * @param dahz z-dimension of viewing window (should = dahx for 90 degree FOV)
+ *  @param ipo camera position
+ *  @param ist camera's unit RIGHT vector
+ *  @param ihe camera's unit DOWN vector
+ *  @param ifo camera's unit FORWARD vector
+ *  @param dahx x-dimension of viewing window
+ *  @param dahy y-dimension of viewing window
+ *  @param dahz z-dimension of viewing window (should = dahx for 90 degree FOV)
  */
 void setcamera (dpoint3d *ipo, dpoint3d *ist, dpoint3d *ihe, dpoint3d *ifo,
 					 float dahx, float dahy, float dahz)
@@ -8230,19 +8023,18 @@ extern "C" {
 
 char ptfaces16[43][8] =
 {
-	{0, 0, 0,  0,  0, 0, 0,0} , {4, 0,32,96, 64, 0,32,0} , {4,16,80,112,48, 16,80,0} , {0,0,0,0,0,0,0,0} ,
-	{4,64,96,112, 80,64,96,0} , {6, 0,32,96,112,80,64,0} , {6,16,80, 64,96,112,48,0} , {0,0,0,0,0,0,0,0} ,
-	{4, 0,16, 48, 32, 0,16,0} , {6, 0,16,48, 32,96,64,0} , {6, 0,16, 80,112,48,32,0} , {0,0,0,0,0,0,0,0} ,
-	{0, 0, 0,  0,  0, 0, 0,0} , {0, 0, 0, 0,  0, 0, 0,0} , {0, 0, 0,  0,  0, 0, 0,0} , {0,0,0,0,0,0,0,0} ,
-	{4, 0,64, 80, 16, 0,64,0} , {6, 0,32,96, 64,80,16,0} , {6, 0,64, 80,112,48,16,0} , {0,0,0,0,0,0,0,0} ,
-	{6, 0,64, 96,112,80,16,0} , {6, 0,32,96,112,80,16,0} , {6, 0,64, 96,112,48,16,0} , {0,0,0,0,0,0,0,0} ,
-	{6, 0,64, 80, 16,48,32,0} , {6,16,48,32, 96,64,80,0} , {6, 0,64, 80,112,48,32,0} , {0,0,0,0,0,0,0,0} ,
-	{0, 0, 0,  0,  0, 0, 0,0} , {0, 0, 0, 0,  0, 0, 0,0} , {0, 0, 0,  0,  0, 0, 0,0} , {0,0,0,0,0,0,0,0} ,
-	{4,32,48,112, 96,32,48,0} , {6, 0,32,48,112,96,64,0} , {6,16,80,112, 96,32,48,0} , {0,0,0,0,0,0,0,0} ,
-	{6,32,48,112, 80,64,96,0} , {6, 0,32,48,112,80,64,0} , {6,16,80, 64, 96,32,48,0} , {0,0,0,0,0,0,0,0} ,
-	{6, 0,16, 48,112,96,32,0} , {6, 0,16,48,112,96,64,0} , {6, 0,16, 80,112,96,32,0}
+    {0, 0, 0,  0,  0, 0, 0,0} , {4, 0,32,96, 64, 0,32,0} , {4,16,80,112,48, 16,80,0} , {0,0,0,0,0,0,0,0} ,
+    {4,64,96,112, 80,64,96,0} , {6, 0,32,96,112,80,64,0} , {6,16,80, 64,96,112,48,0} , {0,0,0,0,0,0,0,0} ,
+    {4, 0,16, 48, 32, 0,16,0} , {6, 0,16,48, 32,96,64,0} , {6, 0,16, 80,112,48,32,0} , {0,0,0,0,0,0,0,0} ,
+    {0, 0, 0,  0,  0, 0, 0,0} , {0, 0, 0, 0,  0, 0, 0,0} , {0, 0, 0,  0,  0, 0, 0,0} , {0,0,0,0,0,0,0,0} ,
+    {4, 0,64, 80, 16, 0,64,0} , {6, 0,32,96, 64,80,16,0} , {6, 0,64, 80,112,48,16,0} , {0,0,0,0,0,0,0,0} ,
+    {6, 0,64, 96,112,80,16,0} , {6, 0,32,96,112,80,16,0} , {6, 0,64, 96,112,48,16,0} , {0,0,0,0,0,0,0,0} ,
+    {6, 0,64, 80, 16,48,32,0} , {6,16,48,32, 96,64,80,0} , {6, 0,64, 80,112,48,32,0} , {0,0,0,0,0,0,0,0} ,
+    {0, 0, 0,  0,  0, 0, 0,0} , {0, 0, 0, 0,  0, 0, 0,0} , {0, 0, 0,  0,  0, 0, 0,0} , {0,0,0,0,0,0,0,0} ,
+    {4,32,48,112, 96,32,48,0} , {6, 0,32,48,112,96,64,0} , {6,16,80,112, 96,32,48,0} , {0,0,0,0,0,0,0,0} ,
+    {6,32,48,112, 80,64,96,0} , {6, 0,32,48,112,80,64,0} , {6,16,80, 64, 96,32,48,0} , {0,0,0,0,0,0,0,0} ,
+    {6, 0,16, 48,112,96,32,0} , {6, 0,16,48,112,96,64,0} , {6, 0,16, 80,112,96,32,0}
 };
-
 
 
 #ifdef __cplusplus
@@ -8257,6 +8049,8 @@ char ptfaces16[43][8] =
 //}
 
 static __ALIGN(8) short lightlist[MAXLIGHTS+1][4];
+
+/** Related to fog functions */
 static int64_t all32767 FORCE_NAME("all32767") = 0x7fff7fff7fff7fff;
 
 static void updatereflects (vx5sprite *spr)
@@ -9704,7 +9498,7 @@ void getspr (vx5sprite *s, const char *filnam)
 		{ s->voxnum = getkv6(filnam); s->flags = 0; }
 }
 
-	//Given vector a, returns b&c that makes (a,b,c) orthonormal
+/** Given vector a, returns b&c that makes (a,b,c) orthonormal */
 void genperp (point3d *a, point3d *b, point3d *c)
 {
 	float t;
@@ -9953,12 +9747,12 @@ static void kfadraw (vx5sprite *s)
 
 //--------------------------- KFA sprite code ends ---------------------------
 
-/**
- * Draw a .KV6/.KFA voxel sprite to the screen. Position & orientation are
- * specified in the vx5sprite structure. See VOXLAP5.H for details on the
- * structure.
+/** Draw a .KV6/.KFA voxel sprite to the screen.
+ *  Position & orientation are
+ *  specified in the vx5sprite structure. See VOXLAP5.H for details on the
+ *  structure.
  *
- * @param spr pointer to vx5sprite
+ *  @param spr pointer to vx5sprite
  */
 void drawsprite (vx5sprite *spr)
 {
@@ -10347,6 +10141,22 @@ freezesprcont:;
 }
 
 #endif
+
+/** Free KV6 data
+ * If you generate any sprites using one of the melt* functions, and then
+ * generate mip-maps for it, you can use this function to de-allocate
+ * all mip-maps of the .KV6 safely. You don't need to use this for
+ * kv6data objects that were loaded by getkv6,getkfa, or getspr since
+ * these functions automatically de-allocate them using this function.
+ *
+ * @param kv6 pointer to kv6 voxel sprite
+ */
+void freekv6 (kv6data *kv6)
+{
+	if (kv6->lowermip) freekv6(kv6->lowermip); //NOTE: dangerous - recursive!
+	free((void *)kv6);
+}
+
 
 	//Sprite structure is already allocated
 	//kv6, vox, xlen, ylen are all malloced in here!
@@ -11510,14 +11320,13 @@ void pngoutputpixel (long rgbcol)
 }
 //------------------------- Simple PNG OUT code ends -------------------------
 
-/**
- * Captures a screenshot of the current frame to disk. The current frame
- * is defined by the last call to the voxsetframebuffer function. NOTE:
- * you MUST call this function while video memory is accessible. In
- * DirectX, that means it must be between a call to startdirectdraw and
- * stopdirectdraw.
- * @param fname filename to write to (writes uncompressed .PNG format)
- * @return 0:always
+/** Captures a screenshot of the current frame to disk.
+ *  The current frame is defined by the last call to the voxsetframebuffer function.
+ *  @note You <b>MUST</b> call this function while video memory is accessible. In
+ *  DirectX, that means it MUST only be between a call to startdirectdraw and
+ *  stopdirectdraw.
+ *  @param fname filename to write to (writes uncompressed .PNG format)
+ *  @return 0:always
  */
 long screencapture32bit (const char *fname)
 {
@@ -11532,16 +11341,15 @@ long screencapture32bit (const char *fname)
 	return(0);
 }
 
-/**
- * Generates a cubic panorama (skybox) from the given position
- * This is an old function that is very slow, but it is pretty cool
- * being able to view a full panorama screenshot. Unfortunately, it
- * doesn't draw sprites or the sky.
+/** Generates a cubic panorama (skybox) from the given position.
+ *  This is an old function that is very slow, but it is pretty cool
+ *  being able to view a full panorama screenshot. Unfortunately, it
+ *  doesn't draw sprites or the sky.
  *
- * @param pos VXL map position of camera
- * @param fname filename to write to (writes uncompressed .PNG format)
- * @param boxsiz length of side of square. I recommend using 256 or 512 for this.
- * @return 0:always
+ *  @param pos VXL map position of camera
+ *  @param fname filename to write to (writes uncompressed .PNG format)
+ *  @param boxsiz length of side of square. I recommend using 256 or 512 for this.
+ *  @return 0:always
  */
 long surroundcapture32bit (dpoint3d *pos, const char *fname, long boxsiz)
 {
@@ -11603,12 +11411,13 @@ long surroundcapture32bit (dpoint3d *pos, const char *fname, long boxsiz)
 	return(0);
 }
 
+
 #if 0
-  //This doesn't speed it up and it only makes it crash on some computers :/
+/** @note This doesn't speed it up and it only makes it crash on some computers :/ */
 static inline void fixsse ()
 {
 	static long asm32;
-	#ifdef __GNUC__ //gcc inline asm
+	#if (defined(__GNUC__) && (USEV5ASM != 0))
 	__asm__ __volatile__
 	(
 		".intel_syntax noprefix\n"
@@ -11617,8 +11426,7 @@ static inline void fixsse ()
 		"ldmxcsr	[asm32]\n"
 		".att_syntax prefix\n"
 	);
-	#endif
-	#ifdef _MSC_VER //msvc inline asm
+	#elif (defined(_MSC_VER) && (USEV5ASM != 0))
 	_asm
 	{
 		stmxcsr	[asm32]   //Default is:0x1f80
@@ -11628,21 +11436,6 @@ static inline void fixsse ()
 	#endif
 }
 #endif
-
-/**
- * If you generate any sprites using one of the melt* functions, and then
- * generate mip-maps for it, you can use this function to de-allocate
- * all mip-maps of the .KV6 safely. You don't need to use this for
- * kv6data objects that were loaded by getkv6,getkfa, or getspr since
- * these functions automatically de-allocate them using this function.
- *
- * @param kv6 pointer to kv6 voxel sprite
- */
-void freekv6 (kv6data *kv6)
-{
-	if (kv6->lowermip) freekv6(kv6->lowermip); //NOTE: dangerous - recursive!
-	free((void *)kv6);
-}
 
 void uninitvoxlap ()
 {
