740b16cbde25d19080171c3671a7a44913edef5a
John Ericson <Ericson2314@Yahoo.com>
2015-12-22 18:57:33 -0800

    shell.bat add LLVM

--- shell.bat
+++ shell.bat
@@ -2,6 +2,11 @@
 @rem Fix directory name if incorrect!
 call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 
+@rem SETUP LLVM environment for command line
+@rem Fix directory name if incorrect!
+set INCLUDE=%ProgramFiles%\LLVM\include;%INCLUDE%
+set LIB=%ProgramFiles%\LLVM\lib\;%LIB%
+
 @rem SETUP DirectX 8.x environment for command line
 @rem Fix directory name if incorrect!
 set INCLUDE=%ProgramFiles%\Microsoft DirectX SDK (June 2010)\include;%INCLUDE%
