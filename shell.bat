b3c861e3bd71e0b05328ab859de9e343f63726ef
John Ericson <Ericson2314@Yahoo.com>
2015-12-23 11:11:07 -0800

    Split `makestuf.bat`: `shell.bat` sets env vars, `build.bat` does build
    # Conflicts:
    #	makestuf.bat

---
+++ shell.bat
@@ -0,0 +1,8 @@
+@rem SETUP Visual C 6.0 (SP5) environment for command line
+@rem Fix directory name if incorrect!
+call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+
+@rem SETUP DirectX 8.x environment for command line
+@rem Fix directory name if incorrect!
+set INCLUDE=%ProgramFiles%\Microsoft DirectX SDK (June 2010)\include;%INCLUDE%
+set LIB=%ProgramFiles%\Microsoft DirectX SDK (June 2010)\Lib\x86;%LIB%
